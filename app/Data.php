cd4843e67d39084700f35e9732f9b9a23cea734c
MateusBMP <mbmp@ic.ufal.br>
2021-01-12 00:44:31 -0300

    Atualizando banco de dados

--- app/Data.php
+++ app/Data.php
@@ -6,6 +6,19 @@ class Data
 {
     static $vinhos = [
         "Tinto" => ["Carne", "Vermelha"],
-        "Branco" => ["Carne", "Branca"],
+        "Branco" => ["Carne", "Branca", "peixes", "frutos", "mar"],
+        "Rosé" => ["Carne", "magra", "grelhada", "assada", "gordura"],
+        "Doce" => ["Doce", "queijo"]
+    ];
+
+    static $tons = [
+        "Vermelho-púrpura" => ["maior", "tanicidade", "acidez"],
+        "Vermelho-rubi" => ["equilibrio", "tanacidade", "acidez", "maciez"],
+        "Vermelho-granada" => ["envelhecidos", "macio"],
+        "Vermelho-alaranjado" => ["envelhecidos", "taninos", "macio"],
+        "Amarelo-esverdeado" => ["fresco", "brilhante", "acidez"],
+        "Amarelo-palha" => ["equilibrio", "acidez", "maciez"],
+        "Amarelo-dourado" => ["macio"],
+        "Amarelo-âmbar" => ["licor", "licoroso", "maciez"],
     ];
 }
