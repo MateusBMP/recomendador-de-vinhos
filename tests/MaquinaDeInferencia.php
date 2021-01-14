d93eb5b220a91e75ced518a4737ebdf3c5d0b0d0
MateusBMP <mbmp@ic.ufal.br>
2021-01-14 10:02:43 -0300

    teste atualizado

--- tests/MaquinaDeInferencia.php
+++ tests/MaquinaDeInferencia.php
@@ -2,7 +2,30 @@
 
 require_once(__DIR__."/../vendor/autoload.php");
 
-use App\Controllers;
+use App\Contracts\BaseDeRegras;
+use App\Contracts\SistemaEspecialista;
+use App\Maquinas\EncadeamentoProgressivo;
+use App\Models\Fato;
+use App\Models\Regra;
 
-$controller = new Controllers\Api\RecomendarVinho;
-$controller->render();
+$fatos = [
+    new Fato("A", "verdade", 1),
+    new Fato("B", "verdade", 2),
+];
+$regras = [
+    new Regra([
+        $fatos[0],
+        $fatos[1]
+    ], new Fato("C", "verdade"), 1)
+];
+
+$br = new BaseDeRegras($fatos, $regras);
+$mi = new EncadeamentoProgressivo();
+
+$se = new SistemaEspecialista($br, $mi);
+
+$e_fato = ($se->e_fato(new Fato("C", 'verdade'))) ?  "verdadeiro" : "falso";
+$busca = $se->buscar("C");
+
+echo "C -> verdade é fato?: $e_fato\n";
+echo "Buscar C: {$busca->valor}\n";
